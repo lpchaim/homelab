2633ab6d08305a3d02ef47e2be1a955d203b1c8e
Lucas Chaim <lpchaim@gmail.com>
2023-12-31 13:12:09 -0300

    refactor(terraform-nix): Flake improvements
    Move LXC configurations to legacyPackages so it
    plays nice with `nix flake show`
    
    Dynamically load services from the services folder

--- terraform-nix/flake.nix
+++ terraform-nix/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
     flake-utils.url = "github:numtide/flake-utils";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
@@ -12,34 +13,43 @@
   outputs = { self, flake-utils, nixpkgs, nixpkgs-unstable, nixos-generators, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        commonPkgConfig = {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        pkgsUnstable = import nixpkgs-unstable commonPkgConfig;
-        pkgs = import nixpkgs-unstable commonPkgConfig;
-        makeProxmoxLxc = modules:
+        makePkgs = nixpkgs:
+          import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              allowUnfreePredicate = _: true;
+            };
+          };
+        pkgs = makePkgs nixpkgs;
+        pkgsUnstable = makePkgs nixpkgs-unstable;
+        makeNixosConfig = modules:
           nixpkgs.lib.nixosSystem {
             inherit system;
-            modules = [
-              nixos-generators.nixosModules.proxmox-lxc
-              ./modules/base.nix
-            ] ++ modules;
+            modules = [ nixos-generators.nixosModules.proxmox-lxc ] ++ modules;
+          };
+        makeProxmoxLxc = modules:
+          nixos-generators.nixosGenerate {
+            inherit system modules;
+            format = "proxmox-lxc";
+            pkgs = nixpkgs.legacyPackages.${system};
+            lib = nixpkgs.legacyPackages.${system}.lib;
+            specialArgs = { inherit inputs pkgs system; };
           };
       in {
+        legacyPackages.nixosConfigurations =
+          let
+            servicesPath = ./services;
+            services = builtins.readDir servicesPath;
+          in
+          pkgs.lib.mapAttrs'
+            (file: _: pkgs.lib.nameValuePair
+              (pkgs.lib.removeSuffix ".nix" file)
+              (makeNixosConfig [ "${servicesPath}/${file}" ]))
+            services;
         packages = rec {
           default = base-proxmox-lxc;
-          base-proxmox-lxc = nixos-generators.nixosGenerate {
-            inherit system;
-            modules = [ ./modules/base.nix ];
-            format = "proxmox-lxc";
-            pkgs = nixpkgs.${system};
-            lib = nixpkgs.legacyPackages.${system}.lib;
-          };
-          nixosConfigurations = rec {
-            default = caddy;
-            caddy = makeProxmoxLxc [ ./modules/caddy.nix ];
-          };
+          base-proxmox-lxc = makeProxmoxLxc [];
         };
         devShells.default =
           with pkgsUnstable;
